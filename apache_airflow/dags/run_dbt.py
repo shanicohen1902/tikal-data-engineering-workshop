4b01ca250a834097423fd1a3e12d795a48978bdd
Shani Cohen <166710023+shani-armis@users.noreply.github.com>
2024-09-19 11:02:46 +0300

    add airflow

---
+++ apache_airflow/dags/run_dbt.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 9, 18),
+    'retries': 0,
+    'catchup': False
+}
+
+dag = DAG('dbt_run_exercise', default_args=default_args, schedule_interval='@once')
+
+run_dbt = BashOperator(
+    task_id='run_dbt',
+    bash_command='cd /Users/shani.cohen/dev/dbt-workshop/cost_allocation/cost_allocation && dbt run',
+    dag=dag,
+)
+
+dbt_test = BashOperator(
+    task_id='dbt_test',
+    bash_command='cd /Users/shani.cohen/dev/dbt-workshop/cost_allocation/cost_allocation && dbt run',
+    dag=dag,
+)
+
+run_dbt >> dbt_test
